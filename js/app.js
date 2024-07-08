7170d6edcee6aba21d880e3b707ac44ce884f02f
Dize dev <axiozweb2022@gmail.com>
2024-07-08 00:26:10 -0500

    optenemos las peliculas y las mostramos en pantalla

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const cargarPeliculas = async () => {
+	try {
+		const response = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?language=en-MX&page=1&api_key=8566ac87fd44ddb80115ffdf24e1fdcf`,
+		);
+
+		if (!response.status === 200) return console.log('error del sevidor');
+		if (response.status === 401)
+			return console.log('usuario no autorizado');
+		if (response.status === 404)
+			return console.log('pelicula no encontrada');
+		const { results } = await response.json();
+		mostrasPeliculas(results);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+cargarPeliculas();
+
+const mostrasPeliculas = (data) => {
+	let peliculas = '';
+	data.map((pelicula) => {
+		peliculas += `
+			<div class="pelicula">
+				<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+				<h3 class="titulo">${pelicula.title}</h3>
+			</div>
+		
+		`;
+	});
+	document.getElementById('contenedor').innerHTML = peliculas;
+};
